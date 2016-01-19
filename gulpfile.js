47a939727b2b5ab2e6bf460204016cd12b14adb4
Katherine Ude <Katie.Ude@gmail.com>
2016-01-19 10:59:32 -0600

    saving finished work from class

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,22 @@ var gulp = require('gulp');
 var browserify = require('browserify');
 var babelify = require('babelify');
 var source = require('vinyl-source-stream');
+var watch = require('gulp-watch');
 // gulp.task(nameOfTask, callback);
 // asynchronous callback method
+
+// watching for any changes to ANY javascript file in folder
+// followed by a callback (function)
+// in function tell gulp to re-compile and restart
+watch(['./source/*.js'], function() {
+  console.log('App has been modified; re-compiling');
+  gulp.start('default');
+});
+
 gulp.task('default', function() {
   // console.log('yeaaaaaa boiiiii');
   return browserify('./source/app.js')
-    .transform("bableify", {presets: ["es2015", "react"]})
+    .transform("babelify", {presets: ["es2015", "react"]})
     .bundle()
     .pipe(source('build.js'))
     .pipe(gulp.dest('./build/'))
--- source/app.js
+++ source/app.js
@@ -1,5 +1,20 @@
-// so so lonely
-var app = app || {};
-app.hey = function(){
-  console.log('Heyyyy');
+
+// define an ES6 class called HelloWorld
+class HelloWorld {
+  constructor(name, purpose) {
+    this.attrs = {
+      name: name,
+      purpose: purpose
+    }
+  }
+  // define a toString() method on the class
+  toString() {
+    return 'Hello, world!'
+  }
 }
+
+// instantiate a new instance of HelloWorld
+var sample = new HelloWorld("Katie", "being fabulous");
+// console.log sample's toString() method
+console.log(sample.toString());
+console.log(sample.attrs);
